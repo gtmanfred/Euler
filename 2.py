d667e534439169687015c470dab41ba76a14cd44
Daniel Wallace <daniel.wallace12@gmail.com>
2011-04-15 19:09:55 -0400

    solved

--- 2.py
+++ 2.py
@@ -1,1 +1,14 @@
-def 
+def Euler_2(num = 4000000):
+    ans = 0
+    last = 1
+    i = 1
+    while i < num:
+        if i%2 ==0:
+            ans += i
+        j = i
+        i +=last
+        last = j
+    return ans
+
+if __name__ == '__main__':
+    print(Euler_2())
