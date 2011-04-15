bb3f1142c96890776a4dcab0a5b96e53aff10783
Daniel Wallace <daniel.wallace12@gmail.com>
2011-04-15 18:32:38 -0400

    added 1.py

---
+++ 1.py
@@ -0,0 +1,17 @@
+def val(num = 1000):
+    #aset = set()
+    a =[]
+    for i in range(num):
+        if i%5 ==0 or i%3 ==0:
+            a=a+[i]
+            #   aset.add(i)
+           #    aset.add(i)
+   # print(aset)
+   # ans = 0
+   # while (len(aset)>0):
+    #    print(a = sum(aset))
+       # ans += aset.pop()
+    return sum(a)
+
+if __name__ == '__main__':
+    print(val(1000))
