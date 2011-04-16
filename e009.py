d38fc3f940ea694208dfebd802e5b69687cc4b21
Daniel Wallace <daniel.wallace12@gmail.com>
2011-04-16 18:53:51 -0400

    finished e009.py

---
+++ e009.py
@@ -0,0 +1,10 @@
+import math
+def Euler_9(num=1000):
+    for a in range(1,num):
+        for b in range(1,num):
+            if (a**2+b**2==(num-a-b)**2):
+                ans = a*b*(num-a-b)
+                return ans
+
+if __name__=='__main__':
+    print(Euler_9())
