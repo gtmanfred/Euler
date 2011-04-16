5ff243f7ebfc938886dfcaf5ea355eae6a1eec93
Daniel Wallace <daniel.wallace12@gmail.com>
2011-04-15 20:52:37 -0400

    finished 3.py

--- 3.py
+++ 3.py
@@ -1,2 +1,21 @@
-def Euler_3(num=600851475143)
-    
+def Euler_3(num=600851475143):
+    primes = []
+    i =2 
+    while i <= num:
+        if num%i ==0 and isprime(i):
+            num = num//i
+            primes = primes +[i] 
+            i = 2
+        i += 1
+    return max(primes)
+ 
+def isprime(n):
+    i = 2
+    while i<n:
+        if n%i ==0:
+            return False
+        i += 1
+    return True
+
+if __name__=='__main__':
+    print(Euler_3())
