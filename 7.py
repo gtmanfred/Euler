4ac98ac961467ee3ead5d9c03829867007b026b6
Daniel Wallace <daniel.wallace12@gmail.com>
2011-04-15 23:31:36 -0400

    added and finished 7.py

---
+++ 7.py
@@ -0,0 +1,21 @@
+def Euler_7(n = 10001):
+    i =1 
+    num = 0;
+    while i<n:
+        num +=1
+        if isprime(num):
+            i +=1
+    return num 
+
+def isprime(n):
+        i =2 
+        if (n%2 is 0) or (n%3 is 0):
+            return False
+        while i*i<=n:
+            if n%i ==0:
+                return False
+            i += 1
+        return True
+
+if __name__=='__main__':
+    print(Euler_7())
