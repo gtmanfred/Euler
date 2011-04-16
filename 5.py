3d72bdd92e881c0b69d6834acbc723f100c5051f
Daniel Wallace <daniel.wallace12@gmail.com>
2011-04-16 18:29:51 -0400

    fixed and optimized 5.py

--- 5.py
+++ 5.py
@@ -1,12 +1,3 @@
-'''def gcd(a,b): return b and gcd(b, a % b) or a
-def lcm(a,b): return a * b // gcd(a,b)
-
-def Euler_5(num = 20):
-    n = 1
-    for i in range(1,num+1):
-        n = lcm(n, i)
-    return n
-'''
 import isprime
 def Euler_5(top=20):
     val =1
@@ -15,7 +6,7 @@ def Euler_5(top=20):
             num = i
             while num<=top:
                 num = num*i
-        val *=(num//i)
+            val *=(num//i)
     return val
     
 if __name__=='__main__':
