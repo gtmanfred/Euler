05f41d2b52e5f923207ca8347a3ba2431dc5e8dd
Daniel Wallace <daniel.wallace12@gmail.com>
2011-04-16 20:47:08 -0400

    finished Euler_10

---
+++ e010.py
@@ -0,0 +1,9 @@
+import maths 
+
+def Euler_10(top = 2*10**6):
+    return sum(maths.primes(top))
+
+if __name__=='__main__':
+#    print(Euler_10(5000))
+#    print(Euler_10(500000))
+    print(Euler_10())
