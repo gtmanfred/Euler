5ea5d0f51828c1b3cf69e302ed43c582ff6ba14d
Daniel Wallace <daniel.wallace12@gmail.com>
2011-04-17 00:06:20 -0400

    finished and added e012.py

---
+++ e012.py
@@ -0,0 +1,15 @@
+import maths
+
+def Euler_12(num=500):
+    facts=[1]
+    i=1
+    b=True
+    while b:
+        n = i*(i+1)//2
+        if maths.num_factors(n)>num:
+            b=False
+            return n
+        i +=1
+
+if __name__=='__main__':
+    print(Euler_12())
