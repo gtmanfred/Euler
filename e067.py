98521504ae28194a18bd575581bb6afac441c231
Daniel Wallace <daniel.wallace12@gmail.com>
2011-04-17 20:25:09 -0400

    finished e067.py triangle in triangle67.py

---
+++ e067.py
@@ -0,0 +1,17 @@
+import triangle67  
+def Euler_67():
+    return tot(triangle67.tri)
+
+def tot(triangle):
+    while len(triangle)>1:
+        reducetri(triangle)
+    return triangle[0][0]
+
+def reducetri(triangle):
+    lastrow=triangle[-1]
+    for index in range(len(triangle)-1):
+        triangle[-2][index] += max(lastrow[index:index + 2])
+    del triangle[-1]
+
+if __name__=='__main__':
+    print(Euler_67())
