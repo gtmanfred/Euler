ecacda5ced509aac00b7c644f40c69ef649eb3b8
Daniel Wallace <daniel.wallace12@gmail.com>
2011-04-18 17:06:53 -0400

    added e027.py

---
+++ e027.py
@@ -0,0 +1,46 @@
+'''
+Euler published the remarkable quadratic formula:
+
+    n² + n + 41
+
+    It turns out that the formula will produce 40 primes for the consecutive
+    values n = 0 to 39. However, when n = 40, 402 + 40 + 41 = 40(40 + 1) + 41
+    is divisible by 41, and certainly when n = 41, 41² + 41 + 41 is clearly
+    divisible by 41.
+
+    Using computers, the incredible formula  n²  79n + 1601 was discovered,
+    which produces 80 primes for the consecutive values n = 0 to 79. The
+    product of the coefficients, 79 and 1601, is 126479.
+
+    Considering quadratics of the form:
+
+    n² + an + b, where |a| < 1000 and |b| < 1000
+
+    where |n| is the modulus/absolute value of n
+    e.g. |11| = 11 and |4| = 4
+    Find the product of the coefficients, a and b, for the quadratic expression
+    that produces the maximum number of primes for consecutive values of n,
+    starting with n = 0.
+'''
+import maths
+primes = maths.primesd(100000)
+def Euler_27(numa=1000,numb=1000):
+    big = 0
+    for a in range(-numa,numa):
+        for b in range(-numb,numb):
+            n = 0
+            val = n**2+n*a+b
+            if maths.isprime(val):
+                num = 0
+                while maths.isprime(val):
+                    num+=1
+                    n+=1
+                    val = n**2+n*a+b
+                if big<n:
+                    big = n
+                    biga = a
+                    bigb = b
+    return [bigb* biga]
+
+if __name__=='__main__':
+    print(Euler_27())
