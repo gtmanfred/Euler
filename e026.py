919f60c37919bf0c82288bbc5baa90058bc625e4
Daniel Wallace <daniel.wallace12@gmail.com>
2011-04-18 18:58:58 -0400

    added e026.py

---
+++ e026.py
@@ -0,0 +1,41 @@
+'''
+A unit fraction contains 1 in the numerator. The decimal representation of the
+unit fractions with denominators 2 to 10 are given:
+
+    1/2 =   0.5
+    1/3 =   0.(3)
+    1/4 =   0.25
+    1/5 =   0.2
+    1/6 =   0.1(6)
+    1/7 =   0.(142857)
+    1/8 =   0.125
+    1/9 =   0.(1)
+    1/10    =   0.1
+    Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can
+    be seen that 1/7 has a 6-digit recurring cycle.
+
+    Find the value of d  1000 for which 1/d contains the longest recurring
+    cycle in its decimal fraction part.
+'''
+
+def Euler_26(num=1000):
+    maxc = 0
+    ret = 0
+    for i in range(1,num):
+        c = cycle(i)
+        if c>maxc:
+            ret = i
+            maxc = c
+        i+=1
+    return ret
+
+def cycle(num):
+    i = 1
+    if num%2==0:return cycle(num//2)
+    if num%5==0:return cycle(num//5)
+    while True:
+        if (pow(10,i)-1)%num==0: return i
+        else:i+=1
+
+if __name__=='__main__':
+    print(Euler_26())
