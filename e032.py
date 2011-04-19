75881a89bc0da6679c6b4f3c332a41044e6999e3
Daniel Wallace <daniel.wallace12@gmail.com>
2011-04-19 13:09:13 -0400

    finished e032.py

---
+++ e032.py
@@ -0,0 +1,22 @@
+from maths import ispan
+
+def Euler_32():
+    a = set()
+    l = 0
+    t = 0
+    for x in range(1,100):
+        if x%10==0:print(x)
+        for y in range(x,2000):
+            test = str(x)+str(y)+str(x*y)
+            if len(test)==9:
+                if ispan(test):a.add(x*y);t +=1
+                length = len(a)
+                if length>l:print(a);l = length
+        if t==9:break
+    print(a)
+    ret = 0
+    while a:
+        ret+=a.pop()
+    return ret
+if __name__=='__main__':
+    print(Euler_32())
