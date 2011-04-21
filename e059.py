bd468bbb359743df5eba821cb7b035fb53fb5070
Daniel Wallace <daniel.wallace12@gmail.com>
2011-04-21 16:02:56 -0400

    finished Euler 59

---
+++ e059.py
@@ -0,0 +1,28 @@
+from cipher1 import cipher
+def Euler_59():
+    string =  'abcdefghijklmnopqrstuvwxyz'
+    pwrds = [i+j+k for i in string for j in string for k in string]
+    for p in pwrds:    
+        pwrd = [ord(a) for a in p]
+        l = len(pwrd)
+        nums = [c^pwrd[x%l] for x,c in enumerate(cipher)]
+        text = ''.join(chr(f) for f in nums)
+        if text.find(' the ')>0:return sum(nums)
+    '''
+    for num in aset:
+        for i in range(100):
+            if num^i==ord(' '):
+                x = [c^i for c in test]
+                y = ''.join(chr(p) for p in x)
+                y = y.lower()
+                n = y.find('the')
+                print(y)
+                if n>0:print(y);print(ord(y[n]))
+                if y.find('\ the')>0:return sum(x)
+    '''
+    return 'nope'
+
+
+if __name__=='__main__':
+    print(Euler_59())
+
