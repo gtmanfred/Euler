bfc28df53be4802dc82760de3bef2a0d5c787341
Daniel Wallace <daniel.wallace12@gmail.com>
2011-05-01 18:55:47 -0400

    editing Euler 60, backup

--- e060.py
+++ e060.py
@@ -7,43 +7,17 @@ from primes import Primes
 p = Primes(10**4)
 def Euler_60():
     for i in p.pList(10**4):
-        p2 = set(j for j in p.pList(10**4) if j>i and
-                p.isPrime(str(j)+str(i)) and p.isPrime(str(i)+str(j))
-        #for j in p[p.index(i):]:
-        for j in p.pList(minval=i):
-            a = p.isPrime(int(str(i)+str(j)))
-            b = p.isPrime(int(str(j)+str(i)))
-            if a and b:
-                #for k in p[p.index(j):]:
-                for k in p.pList(minval=j):
-                    a = p.isPrime(int(str(k)+str(i)))
-                    b = p.isPrime(int(str(k)+str(j)))
-                    c = p.isPrime(int(str(i)+str(k)))
-                    d = p.isPrime(int(str(j)+str(k)))
-                    if a and b and c and d:
-                        
-                        #for l in p[p.index(k):]:
-                        '''
-                        for l in p.pList(minval=k):
-                            a = p.isPrime(int(str(l)+str(i)))
-                            b = p.isPrime(int(str(l)+str(j)))
-                            c = p.isPrime(int(str(l)+str(k)))
-                            d = p.isPrime(int(str(i)+str(l)))
-                            e = p.isPrime(int(str(j)+str(l)))
-                            f = p.isPrime(int(str(k)+str(l)))
-                            if a and b and c and d and e and f:
-                                print('i:',i,'j:',j,'k:',k,'l:',l)
-                                #for m in p[p.index(l):]:
-                                for m in p.pList(minval=l):
-                                    a = p.isPrime(int(str(m)+str(i)))
-                                    b = p.isPrime(int(str(m)+str(j)))
-                                    c = p.isPrime(int(str(m)+str(k)))
-                                    d = p.isPrime(int(str(m)+str(l)))
-                                    e = p.isPrime(int(str(i)+str(m)))
-                                    f = p.isPrime(int(str(j)+str(m)))
-                                    g = p.isPrime(int(str(k)+str(m)))
-                                    h = p.isPrime(int(str(l)+str(m)))
-                                    if a and b and c and d and e and f and g and h:return [[i,j,k,l,m],sum([i,j,k,l,m])]
-                        '''
+        p1 = set(j for j in p.pList(10**4) if j>i and \
+            p.isPrime(str(j)+str(i)) and p.isPrime(str(i)+str(j)))
+        for j in p1:
+            p2 = set(k for k in p1 if k>j and \
+                p.isPrime(str(j)+str(k)) and p.isPrime(str(k)+str(j)))
+            for k in p2:
+                p3 = set(l for l in p.pList(10**4) if l>k and \
+                    p.isPrime(str(k)+str(l)) and p.isPrime(str(l)+str(k)))
+                return (p3)
+
+
+
 if __name__=='__main__':
  print(Euler_60())
