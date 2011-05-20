2454f4f114b614871dc6312c030babac40dcd4c2
Daniel Wallace <daniel.wallace12@gmail.com>
2011-05-20 14:27:42 -0400

    couldn't speed up, 3min12sec

--- e187.py
+++ e187.py
@@ -8,13 +8,15 @@ def e187(top = 10**8//2):
     #print(len(p))
     alist = primesd3(top)
     count = len(alist)
-    print('start')
+    print('start',time()-ti)
+    b = True
     for i in range(1,len(alist)):
         if i<10**5:print(i)
+        if b:print(alist[i]);b = False
         if i%10**5==0:print(i,time()-ti)
         for j in range(i,len(alist)):
             if alist[i]*alist[j]>10**8:break
             count+=1
-    print(count)
+    return count
 if __name__=='__main__':
     print(e187())
