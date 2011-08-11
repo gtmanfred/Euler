563f497be42f2368e62d79fa75270836bebd4da2
Daniel Wallace <daniel.wallace12@gmail.com>
2011-08-11 13:28:15 -0400

    update

--- Primes.py
+++
@@ -1,9 +0,0 @@
-class Primes:
-    def __init__(self,top = 0):
-        self.MaxPrime = top
-        self.Seive = []
-        self.default = num
-        self.plist = []
-        return
-
-    def 
--- p.py
+++
@@ -1,7 +0,0 @@
-from maths import gcd
-from memorize import memo
-@memo
-def a(n):
-    if n==1:return 7
-    else:
-        return a(n-1)+gcd(n,a(n-1))
